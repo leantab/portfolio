09b0869e87ad93b2f347a9cd060e08aeb6906319
Leandro Tabaj <tabajleandro@gmail.com>
2022-03-20 20:37:38 -0300

    primeros pasos

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <h1 class="logo">Logo</h1>
+
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
